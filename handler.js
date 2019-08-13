212d1090bea876e59e6f026ff31ae7d2a5deb2ad
Raymond Yeh <yehjxraymond@gmail.com>
2019-08-13 17:35:44 +0800

    Cleanup

--- handler.js
+++ handler.js
@@ -1,21 +1,5 @@
 "use strict";
 const { sendMessage, sendPushNotif } = require("./services");
-module.exports.hello = async (event, context, callback) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
 
 module.exports.sendTestMessage = async (event, context, callback) => {
   const result = await sendMessage("HI");
@@ -31,13 +15,7 @@ module.exports.sendTestMessage = async (event, context, callback) => {
   };
 };
 
-const wait = timeout =>
-  new Promise(resolve => {
-    setTimeout(resolve, timeout);
-  });
-
 module.exports.getRequirement = async (event, context, callback) => {
-  await wait(2000);
   return {
     statusCode: 200,
     body: JSON.stringify(
